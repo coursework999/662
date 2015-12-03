1cbdceca50f9428ab095ce0ba1258d04bbc4886c
zhubo <buaazhubo@outlook.com>
2015-12-03 10:34:17 -0800

    delete the bulk update function

--- conf/admin.php
+++ conf/admin.php
@@ -7,7 +7,6 @@
             | <a href="my_admin_order.php" class="clink03">Order Management</a> | <a href="my_admin_user.php" class="clink03">Membership Management</a>
             <BR>
             Additional Functions£º
-            <a href="my_admin_new_goods.php" title="Bulk upload product into database" class="clink03">Bulk Upload Product</a> |
             <a href="my_backup.php" title="In order to gurantee the integrity of Database, please backup database regularly" class="clink03">Database Backup</a> |
             <a href="my_recovery.php" title="recover database using sql file" class="clink03">Database Recovery</a> |
             <a href="db_test.php" title="Test database connection speed" target="_blank" class="clink03">Database Test</a>
